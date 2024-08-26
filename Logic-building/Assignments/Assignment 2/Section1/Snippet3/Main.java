a067084c2e2390260509d23024edf63d7f61b86b
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-08-26 16:47:10 +0530

    Add files via upload

---
+++ Logic-building/Assignments/Assignment 2/Section1/Snippet3/Main.java
@@ -0,0 +1,15 @@
+/*public class Main { 
+    public static int main(String[] args) { 
+        System.out.println("Hello, World!"); 
+        return 0; 
+    }
+}*/	
+//void is missing
+// It is keyword and is used to specify that a method doesn’t return anything
+
+public class Main { 
+    public static void main(String[] args) { 
+        System.out.println("Hello, World!"); 
+        
+    }
+}	
