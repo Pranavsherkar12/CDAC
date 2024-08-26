275ff98eb6254fecccb95df1c7a6bf9ab6922d43
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-08-26 13:48:48 +0530

    Add files via upload

---
+++ Logic-building/Assignments/Assignment 1/5/arofsquare.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class arofsquare{
+		
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	
+	System.out.println("Area of square is: "+n*n);	
+	
+	
+	
+    }
+
+}
