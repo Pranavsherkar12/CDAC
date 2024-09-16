36d5ffb25cfa4889afed26a0e2bb36959a1b6e42
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-09-16 23:13:46 +0530

    Update 1.java

--- OOPJ/Assignments/ShwetaMam/Ass4/Code/1.java
+++ OOPJ/Assignments/ShwetaMam/Ass4/Code/1.java
@@ -1,1 +1,8 @@
+public class WideningConversion {
+    public static void main(String[] args) {
+        int intValue = 10;
+        double doubleValue = intValue;  
+        System.out.println("Widening conversion from int to double: " + doubleValue);
+    }
+}
 
