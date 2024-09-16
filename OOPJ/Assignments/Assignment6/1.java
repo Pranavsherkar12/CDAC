bbbe8c33428e9b39fc8601563aaf20bc5d839143
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-09-16 23:52:07 +0530

    1.java

--- OOPJ/Assignments/Assignment6/1.java
+++ OOPJ/Assignments/Assignment6/1.java
@@ -1,1 +1,27 @@
 
+import java.util.Scanner;
+
+public class ArrayExample1 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        
+
+        System.out.println("Default values of the array:");
+        for (int value : arr) {
+            System.out.println(value);
+        }
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 5 integer values:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+
+        System.out.println("Updated values of the array:");
+        for (int value : arr) {
+            System.out.println(value);
+        }
+    }
+}
