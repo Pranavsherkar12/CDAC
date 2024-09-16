a10e3e0eadc6fc771fd63618eb5bf32583b8dcdd
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-09-16 23:14:25 +0530

    2.java

--- OOPJ/Assignments/ShwetaMam/Ass4/Code/2.java
+++ OOPJ/Assignments/ShwetaMam/Ass4/Code/2.java
@@ -1,1 +1,8 @@
+public class NarrowingConversion {
+    public static void main(String[] args) {
+        double doubleValue = 10.99;
+        int intValue = (int) doubleValue;  
+        System.out.println("Narrowing conversion from double to int: " + intValue);
+    }
+}
 
