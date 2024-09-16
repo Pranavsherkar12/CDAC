f15c52b7225ab52b2e3bc0db24cf768db73584f6
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-09-16 23:14:54 +0530

    3.java

--- OOPJ/Assignments/ShwetaMam/Ass4/Code/3.java
+++ OOPJ/Assignments/ShwetaMam/Ass4/Code/3.java
@@ -1,1 +1,11 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int intValue = 10;
+        double doubleValue = 20.5;
+        float floatValue = 15.5f;
+
+        double result = intValue + doubleValue + floatValue;
+        System.out.println("Result of arithmetic operations (int + double + float): " + result);
+    }
+}
 
