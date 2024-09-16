c4f7204fb60d7a14ea41dd1e3e08918697720a37
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-09-16 23:27:22 +0530

    Create 1.java

---
+++ OOPJ/Assignments/ShwetaMam/Ass5/1.java
@@ -0,0 +1,54 @@
+// Base class: BankAccount
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        } else {
+            System.out.println("Insufficient balance.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    private double withdrawalLimit;
+
+    public SavingsAccount(double balance, double withdrawalLimit) {
+        super(balance);
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount > withdrawalLimit) {
+            System.out.println("Withdrawal amount exceeds limit.");
+        } else {
+            super.withdraw(amount);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        SavingsAccount savings = new SavingsAccount(5000, 1000);
+        savings.deposit(2000);
+        savings.withdraw(3000);  
+        savings.withdraw(1500);  
+        System.out.println("Final Balance: " + savings.getBalance());
+    }
+}
