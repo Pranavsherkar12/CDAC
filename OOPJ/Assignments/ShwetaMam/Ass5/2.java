55cb7f260ed18337301d57babb638d18171a1780
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-09-16 23:27:51 +0530

    Create 2.java

---
+++ OOPJ/Assignments/ShwetaMam/Ass5/2.java
@@ -0,0 +1,35 @@
+class Vehicle {
+    protected String make;
+    protected int year;
+
+    public Vehicle(String make, int year) {
+        this.make = make;
+        this.year = year;
+    }
+
+    public void displayDetails() {
+        System.out.println("Make: " + make + ", Year: " + year);
+    }
+}
+
+class Car extends Vehicle {
+    private String model;
+
+    public Car(String make, int year, String model) {
+        super(make, year);
+        this.model = model;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Model: " + model);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Toyota", 2022, "Corolla");
+        car.displayDetails();
+    }
+}
