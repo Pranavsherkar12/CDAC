5de99abd7d237c1380345c8417820189dd87f919
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-09-28 22:06:18 +0530

    Update 1.java

--- ADS/Assignments/Assignment2/1/1.java
+++ ADS/Assignments/Assignment2/1/1.java
@@ -1,1 +1,28 @@
 
+public class Program1 {
+
+	public static void stars(int n) {
+//Since row = no. enter in the input
+		for (int i = 0; i <= n; i++) {
+//Since column = no of rows printing 
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number:");
+		int num = sc.nextInt();
+
+		stars(num);
+		
+
+	}
+
+}
