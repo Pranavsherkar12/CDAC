fee3a98e810f8e02a8b385d87503301b849f0b63
Pranav Sherkar <124600807+Pranavsherkar12@users.noreply.github.com>
2024-09-29 22:31:20 +0530

    3.java

--- ADS/Assignments/Assignment2/3/3.java
+++ ADS/Assignments/Assignment2/3/3.java
@@ -1,1 +1,50 @@
+import java.util.Scanner;
 
+public class Program3 {
+
+	public static void main1(String[] args) {
+
+		// using replaceAll method
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string:");
+		String str1 = sc.nextLine();
+
+		String n1 = str1.replaceAll("\\s", "");
+
+		System.out.println(n1);
+	}
+
+	public static void main2(String[] args) {
+
+		// using replace method
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string:");
+		String str1 = sc.nextLine();
+
+		String n1 = str1.replace(" ", "");
+
+		System.out.println(n1);
+	}
+
+	public static void main(String[] args) {
+
+		// using Char class inbuilt function
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string:");
+		String str1 = sc.nextLine();
+
+		String op = "";
+
+		for (int i = 0; i < str1.length(); i++) {
+
+			char c = str1.charAt(i);
+
+			if (!Character.isWhitespace(c))
+				op = op + c;
+
+		}
+
+		System.out.println(op);
+	}
+
+}
